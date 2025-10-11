b408c8479b11c0dc00556cfe23d677d89f61221c
Imdad Ul Haq <imdadulhaq.5903@gmail.com>
2025-10-12 00:10:32 +0530

    fixed the fianl file paths

--- Server/server.py
+++ Server/server.py
@@ -1,16 +1,19 @@
 from flask import Flask, request, jsonify, render_template
 import util
 import os
+from flask_cors import CORS
 
-app = Flask(__name__, template_folder='../client', static_folder='../client')
+
+app = Flask(__name__, template_folder='../templates', static_folder='../static')
+CORS(app)
+
 
 
 @app.route('/')
 def index():
     return render_template('app.html')
 
-
-@app.route('/get_location_names')
+@app.route('/get_location_names', methods=['GET'])
 def get_location_names():
     response = jsonify({
         'locations': util.get_location_names()
@@ -18,26 +21,24 @@ def get_location_names():
     response.headers.add('Access-Control-Allow-Origin', '*')
     return response
 
-
 @app.route('/predict_home_price', methods=['POST'])
 def predict_home_price():
-    try:
-        total_sqft = float(request.form['total_sqft'])
-        location = request.form['location']
-        bhk = int(request.form['bhk'])
-        bath = int(request.form['bath'])
+    total_sqft = float(request.form['total_sqft'])
+    location = request.form['location']
+    bhk = int(request.form['bhk'])
+    bath = int(request.form['bath'])
 
-        response = jsonify({
-            'estimated_price': util.get_estimated_price(location, total_sqft, bhk, bath)
-        })
-        response.headers.add('Access-Control-Allow-Origin', '*')
-        return response
-    except Exception as e:
-        return jsonify({'error': str(e)})
+    response = jsonify({
+        'estimated_price': util.get_estimated_price(location, total_sqft, bhk, bath)
+    })
+    response.headers.add('Access-Control-Allow-Origin', '*')
+
+    return response
+
 
 
 if __name__ == "__main__":
-    print("Starting Python Flask Server for Bangalore Home Price Prediction...")
+    print("✅ Starting Flask Server for Bangalore Home Price Prediction...")
     util.load_saved_artifacts()
-    app.run(debug=True)
-
+    port = int(os.environ.get("PORT", 5000))  
+    app.run(host='0.0.0.0', port=port)
