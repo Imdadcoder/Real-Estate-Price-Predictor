966ed568579ce19d71d045be155c62ef30bb651e
Imdad Ul Haq <imdadulhaq.5903@gmail.com>
2025-10-15 23:42:15 +0530

    fixed javascript file

--- Client/app.js
+++ Client/app.js
@@ -1,8 +1,8 @@
 function getBathValue() {
   var uiBathrooms = document.getElementsByName("uiBathrooms");
-  for (var i in uiBathrooms) {
+  for (var i = 0; i < uiBathrooms.length; i++) {
     if (uiBathrooms[i].checked) {
-      return parseInt(i) + 1;
+      return i + 1;
     }
   }
   return -1;
@@ -10,9 +10,9 @@ function getBathValue() {
 
 function getBHKValue() {
   var uiBHK = document.getElementsByName("uiBHK");
-  for (var i in uiBHK) {
+  for (var i = 0; i < uiBHK.length; i++) {
     if (uiBHK[i].checked) {
-      return parseInt(i) + 1;
+      return i + 1;
     }
   }
   return -1;
@@ -20,42 +20,51 @@ function getBHKValue() {
 
 function onClickedEstimatePrice() {
   console.log("Estimate price button clicked");
+
   var sqft = document.getElementById("uiSqft");
   var bhk = getBHKValue();
   var bathrooms = getBathValue();
   var location = document.getElementById("uiLocations");
   var estPrice = document.getElementById("uiEstimatedPrice");
 
-  var url = "/predict_home_price"; // ✅ works locally and on Render
+  
+  var url = "https://bangalore-realestate-pricepredictor.onrender.com/predict_home_price";
 
   $.post(url, {
     total_sqft: parseFloat(sqft.value),
     bhk: bhk,
     bath: bathrooms,
-    location: location.value,
+    location: location.value
   }, function (data, status) {
-    console.log(data.estimated_price);
-    estPrice.innerHTML = "<h2>" + data.estimated_price.toString() + " Lakh</h2>";
-    console.log(status);
+    console.log("Response:", data);
+    if (data && data.estimated_price) {
+      estPrice.innerHTML = "<h2>" + data.estimated_price.toString() + " Lakh</h2>";
+    } else {
+      estPrice.innerHTML = "<h2>Could not fetch price</h2>";
+    }
+  }).fail(function () {
+    estPrice.innerHTML = "<h2>Server Error - Try again</h2>";
   });
 }
 
 function onPageLoad() {
-  console.log("document loaded");
+  console.log("Document loaded");
 
-  var url = "/get_location_names"; // ✅ dynamic for Render
+  
+  var url = "https://bangalore-realestate-pricepredictor.onrender.com/get_location_names";
 
   $.get(url, function (data, status) {
-    console.log("got response for get_location_names request");
-    if (data) {
-      var locations = data.locations;
+    console.log("Got response for get_location_names request:", data);
+    if (data && data.locations) {
       var uiLocations = document.getElementById("uiLocations");
       $('#uiLocations').empty();
-      for (var i in locations) {
-        var opt = new Option(locations[i]);
+      for (var i in data.locations) {
+        var opt = new Option(data.locations[i]);
         $('#uiLocations').append(opt);
       }
     }
+  }).fail(function () {
+    console.log("Error loading locations from backend");
   });
 }
 
